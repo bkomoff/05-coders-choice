025381490fb96a4ba7d33324e44f7906e487c12d
Bryan Komoff <bkomoff@smu.edu>
2017-11-22 23:07:26 -0500

    Moved things around in square and got an early move functionality working.

--- stratego/lib/stratego/squares/squares.ex
+++ stratego/lib/stratego/squares/squares.ex
@@ -1,19 +1,24 @@
 defmodule Stratego.Squares do
     def start_link( {column, row} ) do
-        Agent.start_link( fn -> %{piece: :empty, controlled_by: :no_one} end, name: square_name({column, row}) )
+        Agent.start_link( fn -> empty_square() end, name: square_name({column, row}) )
     end
 
-    def place_piece( player, piece, {column, row} ) do
-        valid_piece?(piece)
-        |> update_square(piece, player, square_name({column, row}))
+    def place_piece( player, piece, {column, row} ) when piece in [ :general, :marshal, :colonel, 
+                                                                    :major,   :captain, :lieutenant,
+                                                                    :sergeant,:miner,   :scout,
+                                                                    :spy,     :bomb,    :flag] do
+        update_square(piece, player, square_name({column, row}))
+    end
+
+    def place_piece( _player, piece, {column, row} ) when piece in [:empty] do
+        update_square( :empty, :no_one, square_name({column, row}))
     end
 
     def move_piece( player, {column, row}, direction) when direction in [:up] do
-        valid_move?({column,row + 1})
-        #Check to see if the square it is moving to is occupied
-        #if so then see what battle outcome will be
-        #Update current square to piece: :empty, controlled_by: :no_one
-        #Update new position with piece; could be your piece or opponents piece
+        from_square = get_square({column,row})
+        piece = Map.get(from_square, :piece)
+        update_square( piece, player, square_name{column+1,row})
+        update_square( :empty, :no_one, square_name{column,row})     
     end
 
     # def move_piece( player, {column, row}, direction) when direction in [:left] do
@@ -33,34 +38,11 @@ defmodule Stratego.Squares do
         String.to_atom("Col#{column},Row#{row}")
     end
 
-    defp update_square( :valid_piece, piece, player, square ) do
+    defp update_square( piece, player, square ) do
         Agent.update( square, fn s -> %{ piece: piece, controlled_by: player }  end )        
     end
 
-    defp update_square( _,_,_,_) do
-        
+    defp empty_square() do
+        %{piece: :empty, controlled_by: :no_one}
     end
-
-    defp valid_piece?(piece) when piece in [ :general, :marshal, :colonel, 
-                                             :major,   :captain, :lieutenant,
-                                             :sergeant,:miner,   :scout,
-                                             :spy,     :bomb,    :flag] do
-        :valid_piece                                             
-    end
-
-    defp valid_piece?(_piece) do
-        :invalid_piece
-    end
-
-    defp valid_move?({column,row}) when is_integer(row) and (row >= 1 and row <= 10) and (is_integer(column) and (column >= 1 and column <= 10)) do
-        :valid_move
-    end
-
-    defp valid_move?({_column,_row}) do
-        :invalid_move
-    end
-
-    defp square_occupied?(square = %{piece: :empty}) do
-        :not_occupied
-    end    
 end
