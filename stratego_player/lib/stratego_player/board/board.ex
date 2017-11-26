a3ee3c395e80bddad028a99dd291352b46edf23c
Bryan Komoff <bkomoff@smu.edu>
2017-11-26 15:35:29 -0500

    some refactoring

---
+++ stratego_player/lib/stratego_player/board/board.ex
@@ -0,0 +1,36 @@
+defmodule Stratego.Board do
+
+    def print_board() do
+        for row <- 10..1, do: print_col(row)
+    end    
+
+    defp print_col(row) do
+        for col <- 1..10, do: print_square(row,col)
+        IO.puts "\n"
+    end
+
+    #Only print square that is either empty or owned by you everything else is a ?
+    defp print_square(row,col) do
+        StrategoPlayer.Impl.get_square({row,col})
+        |> convert_for_display
+        |> IO.write
+    end
+
+    defp convert_for_display(square_state) do
+        case square_state do
+            %{ piece: :empty } -> " O "
+            %{ piece: :marshal } -> " M "
+            %{ piece: :general } -> " G "
+            %{ piece: :colonel } -> " Co "
+            %{ piece: :major } -> " M "
+            %{ piece: :captain } -> " Ca "
+            %{ piece: :lieutenant } -> " L "
+            %{ piece: :sergeant } -> " Se "
+            %{ piece: :miner } -> " M "
+            %{ piece: :scout } -> " Sc "
+            %{ piece: :spy } -> " Sp "
+            %{ piece: :bomb } -> " B "
+            %{ piece: :flag } -> " F "
+        end
+    end
+end
--- stratego_player/lib/stratego_player/impl.ex
+++ stratego_player/lib/stratego_player/impl.ex
@@ -16,12 +16,20 @@ defmodule StrategoPlayer.Impl do
         send_to_game([:new_game, player])
     end
 
-    def place_piece( game, piece, {row,column}) do
-        send_to_game([:place_piece, game, piece, {row,column}])
+    def player_ready(player) do
+        send_to_game([:player_ready, player])
     end
 
-    def move_piece( game, {row,column}, direction) do
-        send_to_game([:move_piece, game, {row,column}, direction])
+    def place_piece( player, piece, {row,column}) do
+        send_to_game([:place_piece, player, piece, {row,column}])
+    end
+
+    def get_square({row,column}) do
+        send_to_game([:get_square, {row,column}])
+    end
+
+    def move_piece({row,column}, direction) do
+        send_to_game([:move_piece, {row,column}, direction])
     end
 
     def game_state() do
